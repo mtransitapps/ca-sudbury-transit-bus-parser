d4496cd7f3ef5c9c89911feb6e77342cdb77759a
Mathieu Méa <mathieu.mea@gmail.com>
2021-02-16 11:28:47 -0500

    Cleanup

--- src/main/java/org/mtransit/parser/ca_sudbury_transit_bus/SudburyTransitBusAgencyTools.java
+++ src/main/java/org/mtransit/parser/ca_sudbury_transit_bus/SudburyTransitBusAgencyTools.java
@@ -1,30 +1,21 @@
 package org.mtransit.parser.ca_sudbury_transit_bus;
 
-import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
-import org.mtransit.parser.CleanUtils;
+import org.mtransit.commons.CharUtils;
+import org.mtransit.commons.CleanUtils;
+import org.mtransit.commons.StringUtils;
+import org.mtransit.parser.ColorUtils;
 import org.mtransit.parser.DefaultAgencyTools;
 import org.mtransit.parser.MTLog;
-import org.mtransit.parser.Pair;
-import org.mtransit.parser.SplitUtils;
-import org.mtransit.parser.SplitUtils.RouteTripSpec;
-import org.mtransit.parser.Utils;
-import org.mtransit.parser.gtfs.data.GCalendar;
-import org.mtransit.parser.gtfs.data.GCalendarDate;
 import org.mtransit.parser.gtfs.data.GRoute;
 import org.mtransit.parser.gtfs.data.GSpec;
 import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.gtfs.data.GTrip;
-import org.mtransit.parser.gtfs.data.GTripStop;
 import org.mtransit.parser.mt.data.MAgency;
 import org.mtransit.parser.mt.data.MRoute;
 import org.mtransit.parser.mt.data.MTrip;
-import org.mtransit.parser.mt.data.MTripStop;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.regex.Matcher;
@@ -37,25 +28,14 @@ import java.util.regex.Pattern;
 // https://www.greatersudbury.ca/image/opendata/gtfs.zip
 public class SudburyTransitBusAgencyTools extends DefaultAgencyTools {
 
-	public static void main(String[] args) {
-		if (args == null || args.length == 0) {
-			args = new String[3];
-			args[0] = "input/gtfs.zip";
-			args[1] = "../../mtransitapps/ca-sudbury-transit-bus-android/res/raw/";
-			args[2] = ""; // files-prefix
-		}
+	public static void main(@NotNull String[] args) {
 		new SudburyTransitBusAgencyTools().start(args);
 	}
 
-	private HashSet<String> serviceIds;
-
+	@NotNull
 	@Override
-	public void start(String[] args) {
-		MTLog.log("Generating Sudbury Transit bus data...");
-		long start = System.currentTimeMillis();
-		this.serviceIds = extractUsefulServiceIds(args, this, true);
-		super.start(args);
-		MTLog.log("Generating Sudbury Transit bus data... DONE in %s.", Utils.getPrettyDuration(System.currentTimeMillis() - start));
+	public String getAgencyName() {
+		return "Sudbury Transit";
 	}
 
 	@Override
@@ -64,34 +44,11 @@ public class SudburyTransitBusAgencyTools extends DefaultAgencyTools {
 	}
 
 	@Override
-	public boolean excludingAll() {
-		return this.serviceIds != null && this.serviceIds.isEmpty();
+	public boolean defaultExcludeEnabled() {
+		return true;
 	}
 
-	@Override
-	public boolean excludeCalendar(GCalendar gCalendar) {
-		if (this.serviceIds != null) {
-			return excludeUselessCalendar(gCalendar, this.serviceIds);
-		}
-		return super.excludeCalendar(gCalendar);
-	}
-
-	@Override
-	public boolean excludeCalendarDate(GCalendarDate gCalendarDates) {
-		if (this.serviceIds != null) {
-			return excludeUselessCalendarDate(gCalendarDates, this.serviceIds);
-		}
-		return super.excludeCalendarDate(gCalendarDates);
-	}
-
-	@Override
-	public boolean excludeTrip(GTrip gTrip) {
-		if (this.serviceIds != null) {
-			return excludeUselessTrip(gTrip, this.serviceIds);
-		}
-		return super.excludeTrip(gTrip);
-	}
-
+	@NotNull
 	@Override
 	public Integer getAgencyRouteType() {
 		return MAgency.ROUTE_TYPE_BUS;
@@ -100,12 +57,12 @@ public class SudburyTransitBusAgencyTools extends DefaultAgencyTools {
 	private static final Pattern DIGITS = Pattern.compile("[\\d]+");
 
 	@Override
-	public long getRouteId(GRoute gRoute) {
-		if (!Utils.isDigitsOnly(gRoute.getRouteShortName())) {
-			Matcher matcher = DIGITS.matcher(gRoute.getRouteShortName());
+	public long getRouteId(@NotNull GRoute gRoute) {
+		if (!CharUtils.isDigitsOnly(gRoute.getRouteShortName())) {
+			final Matcher matcher = DIGITS.matcher(gRoute.getRouteShortName());
 			if (matcher.find()) {
-				int digits = Integer.parseInt(matcher.group());
-				String rsnLC = gRoute.getRouteShortName().toLowerCase(Locale.ENGLISH);
+				final int digits = Integer.parseInt(matcher.group());
+				final String rsnLC = gRoute.getRouteShortName().toLowerCase(Locale.ENGLISH);
 				if (rsnLC.endsWith("r")) {
 					return digits + 180_000L;
 				}
@@ -115,18 +72,19 @@ public class SudburyTransitBusAgencyTools extends DefaultAgencyTools {
 		return Long.parseLong(gRoute.getRouteShortName()); // use route short name as route ID
 	}
 
+	@NotNull
 	@Override
-	public String getRouteLongName(GRoute gRoute) {
-		String routeLongName = gRoute.getRouteLongName();
+	public String cleanRouteLongName(@NotNull String routeLongName) {
 		routeLongName = CleanUtils.cleanSlashes(routeLongName);
 		routeLongName = CleanUtils.cleanStreetTypes(routeLongName);
 		routeLongName = CleanUtils.cleanNumbers(routeLongName);
-		return routeLongName;
+		return CleanUtils.cleanLabel(routeLongName);
 	}
 
+	@NotNull
 	@Override
-	public String getRouteColor(GRoute gRoute) {
-		if ("000000".equals(gRoute.getRouteColor())) {
+	public String getRouteColor(@NotNull GRoute gRoute) {
+		if (ColorUtils.BLACK.equalsIgnoreCase(gRoute.getRouteColor())) {
 			return null;
 		}
 		return super.getRouteColor(gRoute);
@@ -136,6 +94,7 @@ public class SudburyTransitBusAgencyTools extends DefaultAgencyTools {
 
 	private static final String AGENCY_COLOR = AGENCY_COLOR_GREEN;
 
+	@NotNull
 	@Override
 	public String getAgencyColor() {
 		return AGENCY_COLOR;
@@ -143,57 +102,19 @@ public class SudburyTransitBusAgencyTools extends DefaultAgencyTools {
 
 	private static final int OUTBOUND_ID = 1; // like MyBus API, do not change
 	private static final int INBOUND_ID = 2; // like MyBus API, do not change
-	@SuppressWarnings("unused")
-	private static final int LOOP_ID = 3; // like MyBus API, do not change
+	// private static final int LOOP_ID = 3; // like MyBus API, do not change
 
 	private static final String TRANSIT_TERMINAL = "Transit Terminal";
 	private static final String NEW_SUDBURY_CENTRE = "New Sudbury Ctr";
 	private static final String CONISTON = "Coniston";
 	private static final String CAMBRIAN = "Cambrian";
 	private static final String DOW = "Dow";
-	private static final String LAURENTIAN_UNIVERSITY = "Laurentian U";
-	private static final String LAURENTIAN_UNIVERSITY_FR = "U Laurentienne";
 	private static final String CAPREOL = "Capreol";
 	private static final String GRAVEL_DR = "Gravel Dr";
 	private static final String SOUTH_END = "South End";
 
-	private static final HashMap<Long, RouteTripSpec> ALL_ROUTE_TRIPS2;
-
-	static {
-		//noinspection UnnecessaryLocalVariable
-		HashMap<Long, RouteTripSpec> map2 = new HashMap<>();
-		ALL_ROUTE_TRIPS2 = map2;
-	}
-
 	@Override
-	public int compareEarly(long routeId, List<MTripStop> list1, List<MTripStop> list2, MTripStop ts1, MTripStop ts2, GStop ts1GStop, GStop ts2GStop) {
-		if (ALL_ROUTE_TRIPS2.containsKey(routeId)) {
-			return ALL_ROUTE_TRIPS2.get(routeId).compare(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop, this);
-		}
-		return super.compareEarly(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop);
-	}
-
-	@Override
-	public ArrayList<MTrip> splitTrip(MRoute mRoute, GTrip gTrip, GSpec gtfs) {
-		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
-			return ALL_ROUTE_TRIPS2.get(mRoute.getId()).getAllTrips();
-		}
-		return super.splitTrip(mRoute, gTrip, gtfs);
-	}
-
-	@Override
-	public Pair<Long[], Integer[]> splitTripStop(MRoute mRoute, GTrip gTrip, GTripStop gTripStop, ArrayList<MTrip> splitTrips, GSpec routeGTFS) {
-		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
-			return SplitUtils.splitTripStop(mRoute, gTrip, gTripStop, routeGTFS, ALL_ROUTE_TRIPS2.get(mRoute.getId()), this);
-		}
-		return super.splitTripStop(mRoute, gTrip, gTripStop, splitTrips, routeGTFS);
-	}
-
-	@Override
-	public void setTripHeadsign(MRoute mRoute, MTrip mTrip, GTrip gTrip, GSpec gtfs) {
-		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
-			return; // split
-		}
+	public void setTripHeadsign(@NotNull MRoute mRoute, @NotNull MTrip mTrip, @NotNull GTrip gTrip, @NotNull GSpec gtfs) {
 		if (isGoodEnoughAccepted()) {
 			final String tripHeadsign = gTrip.getTripHeadsign();
 			if (mRoute.getId() == 1L) {
@@ -273,6 +194,7 @@ public class SudburyTransitBusAgencyTools extends DefaultAgencyTools {
 				}
 				throw new MTLog.Fatal("%s: Unexpected trip %s!", mRoute.getId(), gTrip.toStringPlus());
 			}
+			//noinspection IfStatementWithIdenticalBranches
 			if (tripHeadsign != null
 					&& (
 					"Transit Terminal".equals(tripHeadsign) //
@@ -291,37 +213,27 @@ public class SudburyTransitBusAgencyTools extends DefaultAgencyTools {
 		throw new MTLog.Fatal("%s: Unexpected trip %s!", mRoute.getId(), gTrip.toStringPlus());
 	}
 
-	private static final Pattern CLEAN_UNIVERISITY = Pattern.compile("((^|\\W)(laurentian university|LU)(\\W|$))", Pattern.CASE_INSENSITIVE);
-	private static final String CLEAN_UNIVERISITY_REPLACEMENT = "$2" + LAURENTIAN_UNIVERSITY + "$4";
-
-	private static final Pattern CLEAN_UNIVERISITY_FR = Pattern.compile("((^|\\W)(universit[e|é] laurentienne|UL)(\\W|$))", Pattern.CASE_INSENSITIVE);
-	private static final String CLEAN_UNIVERISITY_FR_REPLACEMENT = "$2" + LAURENTIAN_UNIVERSITY_FR + "$4";
-
-	private static final Pattern SUDBURY_SHOPPING_CENTER = Pattern.compile("(subdury shopping centre)", Pattern.CASE_INSENSITIVE);
-	private static final String SUDBURY_SHOPPING_CENTER_REPLACEMENT = "Subdury centre";
+	private static final Pattern FIX_SUDBURY_SHOPPING_CENTER = Pattern.compile("(subdury shopping centre)", Pattern.CASE_INSENSITIVE);
+	private static final String FIX_SUDBURY_SHOPPING_CENTER_REPLACEMENT = "Sudbury centre";
 
 	private static final Pattern STARTS_WITH_TO = Pattern.compile("(^.*( to|to) )", Pattern.CASE_INSENSITIVE);
 
+	@NotNull
 	@Override
-	public String cleanTripHeadsign(String tripHeadsign) {
+	public String cleanTripHeadsign(@NotNull String tripHeadsign) {
 		// TODO FIXME in sync with Greater Sudbury Transit provider
-		if (Utils.isUppercaseOnly(tripHeadsign, true, true)) {
-			tripHeadsign = tripHeadsign.toLowerCase(Locale.ENGLISH);
-		}
+		tripHeadsign = CleanUtils.toLowerCaseUpperCaseWords(Locale.ENGLISH, tripHeadsign);
 		tripHeadsign = CleanUtils.keepToAndRemoveVia(tripHeadsign);
 		tripHeadsign = STARTS_WITH_TO.matcher(tripHeadsign).replaceAll(StringUtils.EMPTY);
-		tripHeadsign = SUDBURY_SHOPPING_CENTER.matcher(tripHeadsign).replaceAll(SUDBURY_SHOPPING_CENTER_REPLACEMENT);
-		tripHeadsign = CLEAN_UNIVERISITY.matcher(tripHeadsign).replaceAll(CLEAN_UNIVERISITY_REPLACEMENT);
-		tripHeadsign = CLEAN_UNIVERISITY_FR.matcher(tripHeadsign).replaceAll(CLEAN_UNIVERISITY_FR_REPLACEMENT);
+		tripHeadsign = FIX_SUDBURY_SHOPPING_CENTER.matcher(tripHeadsign).replaceAll(FIX_SUDBURY_SHOPPING_CENTER_REPLACEMENT);
 		tripHeadsign = CleanUtils.CLEAN_AND.matcher(tripHeadsign).replaceAll(CleanUtils.CLEAN_AND_REPLACEMENT);
 		tripHeadsign = CleanUtils.cleanNumbers(tripHeadsign);
 		tripHeadsign = CleanUtils.cleanStreetTypes(tripHeadsign);
-		tripHeadsign = CleanUtils.removePoints(tripHeadsign);
 		return CleanUtils.cleanLabel(tripHeadsign);
 	}
 
 	@Override
-	public boolean mergeHeadsign(MTrip mTrip, MTrip mTripToMerge) {
+	public boolean mergeHeadsign(@NotNull MTrip mTrip, @NotNull MTrip mTripToMerge) {
 		List<String> headsignsValues = Arrays.asList(mTrip.getHeadsignValue(), mTripToMerge.getHeadsignValue());
 		if (mTrip.getRouteId() == 1L) {
 			if (Arrays.asList( //
@@ -391,11 +303,9 @@ public class SudburyTransitBusAgencyTools extends DefaultAgencyTools {
 		throw new MTLog.Fatal("%s: Couldn't merge %s and %s!", mTrip.getRouteId(), mTrip, mTripToMerge);
 	}
 
+	@NotNull
 	@Override
-	public String cleanStopName(String gStopName) {
-		gStopName = CLEAN_UNIVERISITY.matcher(gStopName).replaceAll(CLEAN_UNIVERISITY_REPLACEMENT);
-		gStopName = CLEAN_UNIVERISITY_FR.matcher(gStopName).replaceAll(CLEAN_UNIVERISITY_FR_REPLACEMENT);
-		gStopName = CleanUtils.removePoints(gStopName);
+	public String cleanStopName(@NotNull String gStopName) {
 		gStopName = CleanUtils.cleanNumbers(gStopName);
 		gStopName = CleanUtils.cleanStreetTypes(gStopName);
 		return CleanUtils.cleanLabel(gStopName);
@@ -403,13 +313,13 @@ public class SudburyTransitBusAgencyTools extends DefaultAgencyTools {
 
 	@NotNull
 	@Override
-	public String getStopCode(GStop gStop) {
+	public String getStopCode(@NotNull GStop gStop) {
 		//noinspection deprecation
 		return gStop.getStopId(); // use stop ID as stop code, used by real-time API, do not change
 	}
 
 	@Override
-	public int getStopId(GStop gStop) {
+	public int getStopId(@NotNull GStop gStop) {
 		return super.getStopId(gStop); // used by real-time API, do not change
 	}
 }
